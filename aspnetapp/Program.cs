7705b2f42d98363a9e713c5d3f66dd2dd9e52ec2
JaneConan <JaneConan@live.com>
2022-03-04 17:52:45 +0800

    支持swagger

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -1,9 +1,22 @@
 using aspnetapp;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddControllers();
+//builder.Services.AddRazorPages();
+
+builder.Services.AddMvc();
+
 builder.Services.AddDbContext<CounterContext>();
+
+#region Ìí¼Óswagger×¢ÊÍ
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#endregion
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -19,5 +32,14 @@ app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 app.MapRazorPages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.EnablePersistAuthorization();
+    });
+}
 
 app.Run();
