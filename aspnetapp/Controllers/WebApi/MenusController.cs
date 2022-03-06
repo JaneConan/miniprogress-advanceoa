75bb4b0d41b72061e0d1760e633fbf763d89e348
ConanYao <JaneConan@live.com>
2022-03-06 23:44:30 +0800

    调整项目结构，优化页面

---
+++ aspnetapp/Controllers/WebApi/MenusController.cs
@@ -0,0 +1,108 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using aspnetapp;
+
+namespace aspnetapp.Controllers.WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        private readonly CounterContext _context;
+
+        public MenusController(CounterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Menus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Menu>>> GetMenus()
+        {
+            return await _context.Menus.ToListAsync();
+        }
+
+        // GET: api/Menus/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Menu>> GetMenu(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return menu;
+        }
+
+        // PUT: api/Menus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMenu(int id, Menu menu)
+        {
+            if (id != menu.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(menu).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MenuExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Menus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Menu>> PostMenu(Menu menu)
+        {
+            _context.Menus.Add(menu);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMenu", new { id = menu.id }, menu);
+        }
+
+        // DELETE: api/Menus/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMenu(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            _context.Menus.Remove(menu);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.id == id);
+        }
+    }
+}
